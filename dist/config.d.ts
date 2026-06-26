1f99b45687de3f73404eca376eb717ba9ab729ee
Zakaria Elbahy <zakariaelbahy@MacBook-Pro-de-Zakaria.local>
2026-06-26 16:57:39 +0200

    remove git tracking

--- dist/config.d.ts
+++
@@ -1,8 +0,0 @@
-export declare const config: {
-    PORT: string;
-    DATABASE_URL: string;
-    JWT_SECRET: string;
-    FRONTEND_URL: string;
-    AWS_BUCKET_NAME: string;
-    AWS_REGION: string;
-};
